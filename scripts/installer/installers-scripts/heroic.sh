3c77e466943e0eb28e9ad7c5b9cd633b0a76f837
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2024-12-30 15:43:06 -0400

    small wget fix

--- scripts/installer/installers-scripts/heroic.sh
+++ scripts/installer/installers-scripts/heroic.sh
@@ -6,7 +6,7 @@ if [ -f /usr/bin/heroic ]; then
   return
 fi
 
-sudo wget -o $UserHome/heroic_2.15.2_amd64.deb https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.15.2/heroic_2.15.2_amd64.deb
+sudo wget -O $UserHome/heroic_2.15.2_amd64.deb https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.15.2/heroic_2.15.2_amd64.deb
 sudo nala update
 sudo nala install $UserHome/heroic_2.15.2_amd64.deb -y
 sudo rm $UserHome/heroic_2.15.2_amd64.deb
