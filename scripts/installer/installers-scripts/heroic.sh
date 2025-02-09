28cfb24a4f83d1e11c9ecd33331c894e3ccea8af
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2025-02-09 14:37:36 -0400

    update scripts

--- scripts/installer/installers-scripts/heroic.sh
+++ scripts/installer/installers-scripts/heroic.sh
@@ -6,6 +6,7 @@
 #fi
 
 wget -O $HOME/heroic_2.18.1_amd64.deb https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.18.1/Heroic-2.18.1-linux-amd64.deb
+
 sudo nala update
 sudo nala install $HOME/heroic_2.18.1_amd64.deb -y
 rm $HOME/heroic_2.18.1_amd64.deb
