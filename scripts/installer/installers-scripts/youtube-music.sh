e3e0f0e7a4c47a2540f58b4280277fcf5ba1d427
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2025-05-05 15:16:05 -0400

    youtube music version

--- scripts/installer/installers-scripts/youtube-music.sh
+++ scripts/installer/installers-scripts/youtube-music.sh
@@ -7,7 +7,7 @@ fi
 UserHome="/home/$SUDO_USER"
 UserConfig="$UserHome/.config"
 
-sudo wget -O $UserHome/Downloads/youtube-music.deb https://github.com/th-ch/youtube-music/releases/download/v3.7.5/youtube-music_3.7.5_amd64.deb
+sudo wget -O $UserHome/Downloads/youtube-music.deb https://github.com/th-ch/youtube-music/releases/download/v3.9.0/youtube-music_3.9.0_amd64.deb
 cd $UserHome/Downloads
 sudo nala install youtube-music.deb -y
 rm youtube-music.deb
