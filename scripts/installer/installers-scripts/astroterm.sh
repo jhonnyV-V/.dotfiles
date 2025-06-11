d200a39f644de3c8a099a36dcf5527faf5377edf
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2025-06-10 23:03:18 -0400

    stuff

---
+++ scripts/installer/installers-scripts/astroterm.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget -O astroterm "https://github.com/da-luce/astroterm/releases/latest/download/astroterm-linux-x86_64"
+chmod +x astroterm
+sudo mv astroterm /usr/local/bin/astroterm
