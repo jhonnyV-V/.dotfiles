a8a0df8eee61826b54e7dbf4035a93bc91971387
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2025-09-24 18:08:28 -0400

    fix: always update youtube-music

--- scripts/installer/installers-scripts/youtube-music.sh
+++ scripts/installer/installers-scripts/youtube-music.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -f /usr/bin/youtube-music ]; then
-  exit
-fi
-
-UserHome=$HOME
-UserConfig="$UserHome/.config"
-
-wget -O $UserHome/Downloads/youtube-music.deb https://github.com/th-ch/youtube-music/releases/download/v3.9.0/youtube-music_3.9.0_amd64.deb
-cd $UserHome/Downloads
+wget -O $HOME/Downloads/youtube-music.deb https://github.com/th-ch/youtube-music/releases/download/v3.11.0/youtube-music_3.11.0_amd64.deb
+cd $HOME/Downloads
 sudo nala install youtube-music.deb -y
 rm youtube-music.deb
