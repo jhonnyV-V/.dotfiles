08a068b1b221468189b962c1dbc2770825c76bb6
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2025-11-07 12:34:16 -0400

    chore: update scripts

--- scripts/installer/installers-scripts/nvm.sh
+++ scripts/installer/installers-scripts/nvm.sh
@@ -6,7 +6,7 @@ fi
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
-version=20.19.3
+version=24.11.0
 nvm install $version
 nvm use $version --default
 npm install -g tldr @nestjs/cli pnpm@latest-10
