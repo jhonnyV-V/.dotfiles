79aa4a1d64b33cfcfd335e9dc2637760ead3df3a
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2026-03-21 13:14:45 -0400

    feat: building keepassxc from source

--- scripts/installer/installers-scripts/keepasx.sh
+++ scripts/installer/installers-scripts/keepasx.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-if [ -f /usr/bin/keepassxc ]; then
-  exit
+if [ ! -d "$HOME/Code/cpp/keepassxc/" ]; then
+  git clone git@github.com:keepassxreboot/keepassxc
+  git checkout 2.7.12
 fi
 
-if [ ! -f /etc/apt/sources.list.d/phoerious-ubuntu-keepassxc-jammy.list ]; then
-  sudo add-apt-repository ppa:phoerious/keepassxc
-fi
+cd $HOME/Code/cpp/keepassxc
 
-sudo nala update
-sudo nala install keepassxc -y
+echo "keepassxc: installing dependencies"
+sudo nala install cmake asciidoctor botan libbotan-2-19 libbotan-2-dev libqt5multimedia5 libqt5multimediawidgets5 libqt5multimediaquick5 qtmultimedia5-dev libqt5svg5-dev qttools5-dev libqt5svg5-dev libqt5x11extras5-dev libminizip-dev libpcsclite-dev libqrencode libqrencode-dev libargon2-dev qtbase5-private-dev -y
+echo "keepassxc: building"
+mkdir -p build
+cd build
+cmake -DWITH_XC_ALL=ON -DWITH_TESTS=OFF -DCMAKE_BUILD_TYPE=Release ..
+make -j8
+echo "keepassxc: installing"
+sudo make install
