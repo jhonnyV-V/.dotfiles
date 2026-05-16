ce38bdf0cb3544697784dc4cbbc8453aa7f833c4
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2026-05-15 20:39:40 -0400

    chore: update heroic

--- scripts/installer/installers-scripts/heroic.sh
+++ scripts/installer/installers-scripts/heroic.sh
@@ -5,7 +5,7 @@
 #  exit
 #fi
 
-wget -O $HOME/heroic_2.21.0_amd64.deb https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.21.0/Heroic-2.21.0-linux-amd64.deb
+wget -O $HOME/heroic_2.22.0_amd64.deb https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.22.0/Heroic-2.22.0-linux-amd64.deb
 sudo nala update
-sudo nala install $HOME/heroic_2.21.0_amd64.deb -y
-rm $HOME/heroic_2.21.0_amd64.deb
+sudo nala install $HOME/heroic_2.22.0_amd64.deb -y
+rm $HOME/heroic_2.22.0_amd64.deb
