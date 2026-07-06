c7f9d8ed6ba15a6b8ebeed8d1e6eeb869c0d2bd1
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2026-07-06 10:42:51 -0400

    chore: update youtube music

--- scripts/installer/installers-scripts/youtube-music.sh
+++ scripts/installer/installers-scripts/youtube-music.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget -O $HOME/Downloads/pear-desktop.deb https://github.com/pear-devs/pear-desktop/releases/download/v3.11.0/youtube-music_3.11.0_amd64.deb
+wget -O $HOME/Downloads/pear-desktop.deb https://github.com/pear-devs/pear-desktop/releases/download/v3.12.0/youtube-music_3.12.0_amd64.deb
 cd $HOME/Downloads
 sudo nala install pear-desktop.deb -y
 rm pear-desktop.deb
